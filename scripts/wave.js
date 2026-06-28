a7aa39b48f5db99d85c6888058fdcc0cb68ca2d8
roisakurai <147335776+roisakurai@users.noreply.github.com>
2026-06-28 17:28:52 +0700

    chore: stop tracking scripts directory and apply gitignore

--- scripts/wave.js
+++
@@ -1,8 +0,0 @@
-const bars = document.querySelectorAll(".loader div");
-
-setInterval(() => {
-    bars.forEach(bar => {
-        const h = 20 + Math.random() * 80;
-        bar.style.height = `${h}px`;
-    });
-}, 80);
